82b5176369ef26ffc67b5e01b573446bd66248b4
danielsantana9824 <108958899+danielsantana9824@users.noreply.github.com>
2024-12-14 15:25:28 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const user = require('./user');
 const thought = require('./thought');
+const reaction = require('./reaction');
 
-module.exports = { user, thought };
+module.exports = { user, thought, reaction };
